1d40273f704d6e2a698b1b3db65eaaabf44d512c
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-04-10 21:01:59 +0200

    Update difference.jsx

--- difference.jsx
+++ difference.jsx
@@ -1,31 +1,36 @@
 /*
-Saves the current Photoshopfile as a low res jpg, 
-then imports the saved file and places it in a new layer with its blendmode set to difference,
-and finally it merges all visible layers.
-In most cases the result will need some leveling.
+Saves the current Photoshopfile as a low res jpg,
+then imports the saved file and places it in a new layer with its blendmode set to difference.
+Then it merges all visible layers and applies some levelling.
+This process is then repeated once more.
 */
 
 // stating the obvious, but change this path to match your machine
-var myFile = '~/Desktop/lowResJPEG.jpg';
+var myFile = '~/Documents/Photoshop Difference Script/lowResJPEG.jpg';
 
-// save the lowres jpeg
-function saveJPEG(doc, saveFile, qty) {
-     var saveOptions = new JPEGSaveOptions();
-     saveOptions.embedColorProfile = true;
-     saveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
-     saveOptions.matte = MatteType.NONE;
-     saveOptions.quality = qty;
-     doc.saveAs(saveFile, saveOptions, true);
+for (var i=0; i<2; i++) {
+    // save the lowres jpeg
+    function saveJPEG(doc, saveFile, qty) {
+         var saveOptions = new JPEGSaveOptions();
+         saveOptions.embedColorProfile = true;
+         saveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
+         saveOptions.matte = MatteType.NONE;
+         saveOptions.quality = qty;
+         doc.saveAs(saveFile, saveOptions, true);
+    }
+    saveJPEG( app.activeDocument, new File(myFile), 0);
+
+    // open the lowres jpeg, place it into new layer and set blendmode to difference
+    app.load(new File(myFile));
+    backFile = app.activeDocument;
+    backFile.selection.selectAll();
+    backFile.selection.copy();
+    backFile.close(SaveOptions.DONOTSAVECHANGES);
+    app.activeDocument.paste();
+    app.activeDocument.layers[0].name = "Difference";
+    app.activeDocument.layers[0].blendMode = BlendMode.DIFFERENCE;
+    app.activeDocument.mergeVisibleLayers();
+
+    // apply levels
+    app.activeDocument.layers[0].adjustLevels(0, 45, 1, 0, 255)
 }
-saveJPEG( app.activeDocument, new File(myFile), 0);
-
-// open the lowres jpeg, place it into new layer and set blendmode to difference
-app.load(new File(myFile));
-backFile = app.activeDocument; 
-backFile.selection.selectAll();
-backFile.selection.copy();
-backFile.close(SaveOptions.DONOTSAVECHANGES); 
-app.activeDocument.paste();
-app.activeDocument.layers[0].name = "BackgroundImage";
-app.activeDocument.layers[0].blendMode = BlendMode.DIFFERENCE;
-app.activeDocument.mergeVisibleLayers();
