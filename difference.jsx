f8bda18e6bdc96ba65f5a613acb35a03af6b3709
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-04-11 13:07:12 +0200

    Update difference.jsx

--- difference.jsx
+++ difference.jsx
@@ -8,41 +8,44 @@ The user is being prompted how many times this should be performed.
 // stating the obvious, but change this path to match your machine
 var myFile = '~/Desktop/lowResJPEG.jpg';
 
-// merge all layers when there is more than 1 layer
-if (app.activeDocument.layers.length > 1) {
-    app.activeDocument.mergeVisibleLayers();
-    //app.activeDocument.layers[0].duplicate();
-    //app.activeDocument.layers[1].visible = false;
-}
+// suspend history so there is a single undo for the entire script
+app.activeDocument.suspendHistory("something", "main()");
 
-// function to save as JPEG
-function saveJPEG(doc, saveFile, qty) {
-         var saveOptions = new JPEGSaveOptions();
-         saveOptions.embedColorProfile = true;
-         saveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
-         saveOptions.matte = MatteType.NONE;
-         saveOptions.quality = qty;
-         doc.saveAs(saveFile, saveOptions, true);
+function main () {
+    // merge all layers when there is more than 1 layer
+    if (app.activeDocument.layers.length > 1) {
+        app.activeDocument.mergeVisibleLayers();
     }
 
-// prompt the user
-var count = prompt('How many itterations?', '2');
+    // function to save as JPEG
+    function saveJPEG(doc, saveFile, qty) {
+             var saveOptions = new JPEGSaveOptions();
+             saveOptions.embedColorProfile = true;
+             saveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
+             saveOptions.matte = MatteType.NONE;
+             saveOptions.quality = qty;
+             doc.saveAs(saveFile, saveOptions, true);
+        }
 
-for (var i=0; i<count; i++) {
-    // save the lowres jpeg
-    saveJPEG( app.activeDocument, new File(myFile), 0);
+    // prompt the user
+    var count = prompt('How many itterations?', '2');
 
-    // open the lowres jpeg, place it into new layer and set its blendmode to difference
-    app.load(new File(myFile));
-    backFile = app.activeDocument;
-    backFile.selection.selectAll();
-    backFile.selection.copy();
-    backFile.close(SaveOptions.DONOTSAVECHANGES);
-    app.activeDocument.paste();
-    app.activeDocument.layers[0].blendMode = BlendMode.DIFFERENCE;
-    app.activeDocument.mergeVisibleLayers();
-    app.activeDocument.layers[0].name = "Difference";
+    for (var i=0; i<count; i++) {
+        // save the lowres jpeg
+        saveJPEG( app.activeDocument, new File(myFile), 0);
 
-    // apply levels
-    app.activeDocument.layers[0].adjustLevels(0, 45, 1, 0, 255);
+        // open the lowres jpeg, place it into new layer and set its blendmode to difference
+        app.load(new File(myFile));
+        backFile = app.activeDocument;
+        backFile.selection.selectAll();
+        backFile.selection.copy();
+        backFile.close(SaveOptions.DONOTSAVECHANGES);
+        app.activeDocument.paste();
+        app.activeDocument.layers[0].blendMode = BlendMode.DIFFERENCE;
+        app.activeDocument.mergeVisibleLayers();
+        app.activeDocument.layers[0].name = "Difference";
+
+        // apply levels
+        app.activeDocument.layers[0].adjustLevels(0, 45, 1, 0, 255);
+    }
 }
